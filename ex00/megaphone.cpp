43dae4cf7a8d87720eb99ed55e4e79181516ded9
Victor Zayas <55438812+victor-zayas@users.noreply.github.com>
2023-09-18 10:34:36 +0000

    makefile and main

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 10:13:39 by codespace         #+#    #+#             */
+/*   Updated: 2023/09/18 10:33:52 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+#include <cstring>
+
+int main(int argc, char **argv){
+    if (argc > 1){
+        for (int i = 1; i < argc; i++){
+            for (size_t j = 1; j < std::strlen(argv[i]); j++)
+                std::cout << (char)std::toupper(argv[i][j]);
+        }
+    }
+    else
+        std::cout << "* LOUD AND UNEBEARABKE FEEDBACK NOISE *" << std::endl;
+}
