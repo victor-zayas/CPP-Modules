e4d5e4d5d7a5560a7a0811f4f7b871bbb138be1d
Victor Zayas salvarrey <vzayas-s@c4r5s6.42urduliz.com>
2023-09-20 18:15:11 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -16,10 +16,10 @@
 int main(int argc, char **argv){
     if (argc > 1){
         for (int i = 1; i < argc; i++){
-            for (size_t j = 1; j < std::strlen(argv[i]); j++)
+            for (size_t j = 0; j < std::strlen(argv[i]); j++)
                 std::cout << (char)std::toupper(argv[i][j]);
         }
     }
     else
-        std::cout << "* LOUD AND UNEBEARABKE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
