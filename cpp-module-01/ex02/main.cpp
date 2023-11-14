9d5cd575234f748a7ac115c85df72a8fca9de75a
Victor Zayas <55438812+victor-zayas@users.noreply.github.com>
2023-11-14 12:23:51 +0000

    ex02 done

---
+++ cpp-module-01/ex02/main.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+
+int main(void) {
+    // string
+    std::string brain = "HI THIS IS BRAIN";
+    // pointer
+    std::string *ptr = &brain;
+    // reference
+    std::string &ref = brain;
+
+    std::cout << "Memory address" << std::endl;
+
+    std::cout << "String: " << &brain << std::endl;
+    std::cout << "Pointer: " << ptr << std::endl;
+    std::cout << "Reference: " << &ref << std::endl;
+
+    std::cout << "Content" << std::endl;
+
+    std::cout << "String: " << brain << std::endl;
+    std::cout << "Pointer: " << *ptr << std::endl;
+    std::cout << "Reference: " << ref << std::endl;
+}
