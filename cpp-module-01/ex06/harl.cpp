1daac12c7f23c74dadcafa93caa838f39fd217d8
Victor Zayas salvarrey <vzayas-s@c3r7s3.42urduliz.com>
2023-11-16 14:25:11 +0100

    ex06 done

---
+++ cpp-module-01/ex06/harl.cpp
@@ -0,0 +1,47 @@
+
+#include "harl.hpp"
+
+void	Harl::debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void) {
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level) {
+	std::string	complainlvl[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int i = -1;
+	// void	(Harl::*harlcomplain[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	while(++i < 4) {
+		if (level == complainlvl[i])
+			break ;
+	}
+	switch (i)
+	{
+	case 0:
+		std::cout << "[DEBUG]" << std::endl;
+		this->debug();
+	case 1:
+		std::cout << "[INFO]" << std::endl;
+		this->info();
+	case 2:
+		std::cout << "[WARNING]" << std::endl;
+		this->warning();
+	case 3:
+		std::cout << "[ERROR]" << std::endl;
+		this->error();
+		break;
+	default:
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
---
+++ cpp-module-01/ex06/main.cpp
@@ -0,0 +1,12 @@
+
+#include"harl.hpp"
+
+int	main(int argc, char **argv)
+{
+	Harl	harl;
+
+	if (argc != 2)
+		return(std::cout << "Error: too many arguments" << std::endl, 1);
+	harl.complain(argv[1]);
+	return (0);
+}
