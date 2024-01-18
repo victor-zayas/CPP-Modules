eeac0137a7d55799e787f9d0b561f9781ab49166
Victor Zayas salvarrey <vzayas-s@c3r8s4.42urduliz.com>
2024-01-18 13:01:43 +0100

    WIP

--- cpp-module-04/ex03/main.cpp
+++ cpp-module-04/ex03/main.cpp
@@ -1,8 +1,32 @@
+#include "IMateriaSource.hpp"
 #include "AMateria.hpp"
 #include "ICharacter.hpp"
 #include "Ice.hpp"
 #include "Cure.hpp"
 
-int	main(void) {
+int main() {
+	std::cout << std::endl;
+	IMateriaSource *src = new MateriaSource();
+	ICharacter *bob = new Character("bob");
+	ICharacter *me = new Character("me");
 
+	std::cout << std::endl;
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	
+	std::cout << std::endl;
+	AMateria *tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	
+	std::cout << std::endl;
+	me->use(0, *bob);
+	me->use(1, *bob);
+	
+	std::cout << std::endl;
+	delete bob;
+	delete me;
+	delete src;
 }
