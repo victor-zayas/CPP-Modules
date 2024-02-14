550fde046566eeb2f3bb3b24f7d0c3a390d7399c
Victor Zayas <55438812+victor-zayas@users.noreply.github.com>
2024-02-14 13:21:47 +0000

    ex00 done

---
+++ cpp-module-07/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chain1";
+    std::string d = "chain2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+}
