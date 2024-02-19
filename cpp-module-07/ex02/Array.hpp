c19e3c460adfacdff9d9cbf6d9bfdc35c9d1ef60
Victor Zayas <55438812+victor-zayas@users.noreply.github.com>
2024-02-19 11:11:12 +0000

    cpp7 done

--- cpp-module-07/ex02/Array.hpp
+++ cpp-module-07/ex02/Array.hpp
@@ -3,7 +3,7 @@
 
 #include <stdexcept>
 
-template<typename T>
+/* template<class T>
 class Array {
 private:
     T   *_elements;
@@ -15,8 +15,21 @@ public:
     Array<T> &operator=(const Array<T> &other);
     ~Array();
 
-    T   &operator[](unsigned int index) const;
+    T   &operator[](unsigned int index);
     unsigned int size() const;
+}; */
+
+template <class T>
+class Array {
+public:
+	size_t	_size;
+	T		*_array;
+	Array<T>() { this->_array = new T[0]; this->_size = 0; };
+	Array<T>(unsigned int _numberOfElements) { this->_array = new T[_numberOfElements]; this->_size = _numberOfElements; };
+	Array<T>(const Array<T> &_copy) { _size = _copy._size; this->_array = new T[_size]; for (size_t index = 0; index < this->_size; index++) this->_array[index] = _copy._array[index]; };
+	T	&operator=(const Array<T> &_copy) { this->_array = new T[this->_size]; for (size_t index; index < this->_size; index++) this->_array[index] = _copy._array[index]; return (*this); };
+	T	&operator[](int _reference) { if (_reference < 0) throw std::exception(); if (_reference > (int)this->_size) throw std::exception(); return(this->_array[_reference]); };
+	size_t	size(void) const { return (this->_size); };
 };
 
 #endif
--- cpp-module-07/ex02/main.cpp
+++ cpp-module-07/ex02/main.cpp
@@ -1,25 +1,67 @@
 #include <iostream>
+#include <cstdlib>
 #include "Array.hpp"
 
-int main() {
-    // Testing default constructor
-    Array<int> arr1;
-    std::cout << "arr1 size: " << arr1.size() << std::endl;
-
-    // Testing parameterized constructor
-    Array<int> arr2(5);
-    std::cout << "arr2 size: " << arr2.size() << std::endl;
-
-    // Testing copy constructor and operator=
-    Array<int> arr3 = arr2;
-    std::cout << "arr3 size: " << arr3.size() << std::endl;
-
-    // Testing subscript operator and out of bounds exception
-    try {
-        std::cout << "arr2[10]: " << arr2[10] << std::endl;
-    } catch (std::exception& e) {
-        std::cerr << "Exception caught: " << e.what() << std::endl;
-    }
-
-    return 0;
+#define MAX_VAL 4
+int main(int, char**)
+{
+	Array<int> numbers((unsigned int)MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try{
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		std::cout << "should print this" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try{
+		Array<int> n(0);
+		std::cout << "array of empty size" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	delete [] mirror;//
+	return 0;
 }
